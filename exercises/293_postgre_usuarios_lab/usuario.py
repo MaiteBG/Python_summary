866aec750e080bc1b27a44772749ae9b17842850
Maite Bernaus <maite.bernaus@gmail.com>
2025-04-08 11:21:12 +0200

    Implementacion de las otras clases del ejercicio

--- exercises/293_postgre_usuarios_lab/usuario.py
+++ exercises/293_postgre_usuarios_lab/usuario.py
@@ -1,42 +1,32 @@
-# Crear objectos de entidad Usuario
 class Usuario:
-
-    def __init__(self, id_usuario=None, username=None, password = None):
+    def __init__(self, id_usuario=None, username=None, password=None):
         self._id_usuario = id_usuario
         self._username = username
         self._password = password
 
-
     def __str__(self):
-        return f''' Id usuario: {self._id_usuario},
-                        Username: {self._username},
-                        Password: {self._password}
-            
-        '''
+        return f'Usuario: {self._id_usuario} {self._username} {self._password}'
 
     @property
     def id_usuario(self):
-        return  self._id_usuario
+        return self._id_usuario
+
     @id_usuario.setter
-    def id_usuario(self,id_usuario ):
+    def id_usuario(self, id_usuario):
         self._id_usuario = id_usuario
 
     @property
     def username(self):
-        return  self._username
+        return self._username
+
     @username.setter
-    def username(self,username ):
+    def username(self, username):
         self._username = username
 
     @property
     def password(self):
-        return  self._password
+        return self._password
+
     @password.setter
-    def password(self,password ):
+    def password(self, password):
         self._password = password
-
-
-
-if __name__ == '__main__':
-    user1 = Usuario(1,"Carlos", "1234")
-    print(user1)
